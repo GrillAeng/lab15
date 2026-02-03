289dec7ca1ee808e434cce6ed4bf686ac530ab12
Thada <skill11888@gmail.com>
2026-02-03 16:59:29 +0700

    lab15_1 completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,24 @@
-include <iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-
-	
-	
+    int a=5;
+    char b='A';
+    char &c=b;
+    int *x = &a,**z = &x;
+	char *y = &b;
+    cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z << "\n";
+	cout << &a << " " << (void *) &b << " " << (void *) &c << " " << x << " " << &y << " " << &z << "\n";
+	c = 'F';
+	cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z << "\n";
+	*y = 'W';
+	cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z << "\n";
+	*x = 6;
+	x = &a;
+	cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z << "\n";
+	**z = 7;
+	z = &x;
+	cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z << "\n";
 	return 0;
 }
